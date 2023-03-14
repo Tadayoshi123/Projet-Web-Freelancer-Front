70d201e3bbcd388c2597ff9814f5a96b00a8899e
Vincent Zerbib <contact@zelabstudio.com>
2023-03-14 10:04:14 +0100

    save work

--- src/pages/account/profil/index.js
+++ src/pages/account/profil/index.js
@@ -2,8 +2,10 @@ import { useEffect, useState, useContext } from "react";
 import UserContext from "@/context/UserContext";
 import {useRouter} from "next/router";
 import useFetch from "@/hooks/useFetch";
+import Input from "@/components/UI/Input";
 import Button from "@/components/UI/Button";
 import Modal from "@/components/UI/Modal";
+import Loading from "@/components/UI/Loading";
 
 const Index = () => {  
 
@@ -11,13 +13,15 @@ const Index = () => {
 
   const { isLogged, user } = useContext(UserContext);
 
-  console.log(user);
-
   const [token, setToken] = useState();
+
+  const [userForm, setUserForm] = useState();
 
   const [isOpen , setIsOpen] = useState(false);
 
   const { data, error, loading, fetchData } = useFetch({ url:"/user", method:"GET", body:null, token:token });
+
+  const {data: dataUpdate, error:errorUpdate, loading:loadingUpdate, fetchData:fetchDataUpdate} = useFetch({url:"/user", method:"PUT", body:userForm, token:token})
 
   useEffect(() => {
     if (typeof window != undefined) {
@@ -29,15 +33,68 @@ const Index = () => {
   }, [router.isReady, token])
 
   useEffect(() => {
-    console.log(data);
-  },[data])
+    setUserForm(data.user)
+  }, [data]);
+  
+  useEffect(() => {
+    if (dataUpdate.success) {
+      setIsOpen(false);
+      fetchData();
+    }
+  }, [dataUpdate]);
+
+  if (loading || loadingUpdate) return <Loading />
+
+  if (error) console.log(error);
+  if (errorUpdate) console.log(errorUpdate);
+
+  const handleChange = (e) => {
+    setUserForm({ ...userForm, [e.target.name]: e.target.value })
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    fetchDataUpdate();
+    if (dataUpdate.success) {
+      setIsOpen(false);
+    }
+  }
   
   return (
     <div>
       {
         isOpen && (
-          <Modal title="Modifier mon profil">
-            {/* children props ici */}
+          <Modal title="Modifier mon profil" closeModal={()=>setIsOpen(false)}>
+            <form onSubmit={(e) => {submitForm(e)}}>
+              <Input 
+              label="firstName" 
+              type="text" 
+              name="firstName" 
+              value={userForm.firstName}
+              isRequired={true}
+              placeholder="enter your firstName"
+              onChange={(e) => handleChange(e)}
+              />
+              <Input 
+              label="lastName" 
+              type="text" 
+              name="lastName" 
+              value={userForm.lastName}
+              isRequired={true}
+              placeholder="enter your lastName"
+              onChange={(e) => handleChange(e)}
+              />
+              <Input 
+              label="email" 
+              type="text" 
+              name="email" 
+              value={userForm.email}
+              isRequired={true}
+              placeholder="enter your email"
+              onChange={(e) => handleChange(e)}
+              />
+              <Button type="submit" title="modifier" className="btn__primary"/>
+           </form>
           </Modal>
         )
       }
